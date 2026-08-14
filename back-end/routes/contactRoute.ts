0feb270a5b000ac604d68b875d8f900843daa311
Yathavan <yathavanyathu27@gmail.com>
2026-08-14 13:33:16 +0530

    fix: Add missing .js extension in contactRoute

--- back-end/routes/contactRoute.ts
+++ back-end/routes/contactRoute.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { submitContact } from '../controllers/contactController';
+import { submitContact } from '../controllers/contactController.js';
 
 const router = express.Router();
 
